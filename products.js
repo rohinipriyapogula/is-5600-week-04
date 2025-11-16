1d85992f78c45a0e28ceb7402e5cf876590516a4
rohinipriyapogula <rohinipriyapogula2002@gmail.com>
2025-11-16 06:44:18 +0000

    LAB4

--- products.js
+++ products.js
@@ -1,28 +1,7 @@
 const fs = require('fs').promises
 
 const path = require('path')
-const express = require('express')
 
-// Set the port
-const port = process.env.PORT || 3000
-// Boot the app
-const app = express()
-// Register the public directory
-app.use(express.static(__dirname + '/public'));
-// register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
-// Boot the server
-app.listen(port, () => console.log(`Server listening on port ${port}`))
-
-/**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-}
 
 const productsFile = path.join(__dirname, 'data/full-products.json')
 
@@ -31,20 +10,11 @@ const productsFile = path.join(__dirname, 'data/full-products.json')
 
 /**
  * List all products
- * @param {object} req
- * @param {object} res
  * @returns {Promise<Array>}
  */
-async function listProducts(req, res) {
-  const productsFile = path.join(__dirname, 'data/full-products.json')
-  try {
 async function list (options = {}) {
     const { offset = 0, limit = 25, tag } = options
     const data = await fs.readFile(productsFile)
-    res.json(JSON.parse(data))
-  }
- } catch (err) {
-    res.status(500).json({ error: err.message })
 
     return JSON.parse(data)
     .filter(product => {
@@ -74,5 +44,4 @@ async function get (id) {
   module.exports = {
     list,
     get
-  }
   }
